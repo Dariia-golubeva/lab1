52434582333e10e4417ecdee4826caeb6127f64f
Dashenka <21st.golubeva.d@pdu.by>
2023-09-16 23:27:36 +0300

    change background

---
+++ src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  latitude = 55.4863046;
+  longitude = 28.7629755;
+}
